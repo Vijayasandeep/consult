0161d7a8c429ba97bd97837cf8cdee0065b6b352
Vijayasandeep <108604862+Vijayasandeep@users.noreply.github.com>
2025-03-13 21:32:29 +0530

    Add files via upload

---
+++ backend/src/main/java/com/example/atlast/AtlastApplication.java
@@ -0,0 +1,13 @@
+package com.example.atlast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtlastApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/atlast/AtlastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atlast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
